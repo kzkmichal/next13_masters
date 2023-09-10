75799349b56c80797c201b9256031b1eb45063e6
Michal <michal.kozak@ttc.com>
2023-09-10 20:51:59 +0200

    N13M-02 add data-testid

--- src/app/components/organisms/ProductList.tsx
+++ src/app/components/organisms/ProductList.tsx
@@ -4,7 +4,10 @@ import { type ProductType } from "@/app/types/types";
 
 const ProductList = ({ products }: { products: ProductType[] }) => {
 	return (
-		<ul className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
+		<ul
+			data-testid="products-list"
+			className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4"
+		>
 			{products?.map((item, index) => (
 				<ProductListItem key={index} {...item} />
 			))}
